4f2b3e726f0779373f655ae97d57adcd60458f51
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-04 21:54:13 +0530

    settings:defines

---
+++ settings.py
@@ -0,0 +1,34 @@
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+class Settings(BaseSettings):
+    APP__NAME: str = "PIKA AI"
+    APP_VERSION: str = "1.0.0"
+    DEBUG: bool = True
+    SECRET_KEY: str = ""
+    ALGORITHM: str = "HS256"
+    ACCES_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    DATABASE_URL: str = ""
+    
+    REDIS_URL: str = ""
+    
+    INSTAGRAM_CLIENT_ID: Optional[str] = None
+    INSTAGRAM_CLIENT_SECRET: Optional[str] = None
+    TWITTER_API_KEY: Optional[str] = None
+    TWITTER_API_SECRET: Optional[str] = None
+    TWITTER_BEARER_TOKEN: Optional[str] = None
+    TIKTOK_CLIENT_KEY: Optional[str] = None
+    TIKTOK_CLIENT_SECRET: Optional[str] = None
+    
+    ELEVENLABS_API_KEY: Optional[str] = None
+    OPENAI_API_KEY: Optional[str] = None
+    HUGGINGFACE_TOKEN: Optional[str] = None
+    
+    CORS_ORIGINS: list = ["http:localhost:3000" "http://localhost:3001"]
+    
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+    
+settings = Settings()

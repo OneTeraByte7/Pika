363d67cd419037b24128bd9beb93046afbe930a9
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-05 13:55:33 +0530

    enum

--- schemas.py
+++ schemas.py
@@ -1,4 +1,11 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import Optional, Dict, List, Any
 from datetime import datetime
+from enum import Enum
 
+class Platfrom(str, Enum):
+    INSTAGRAM = "instagram"
+    TWITTER = "twitter"
+    TIKTOK = "tiktok"
+    
+    

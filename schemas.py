1961c64980fb60ec820a1b1d375b10d6c66eb2ec
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-05 14:16:40 +0530

    more:functions

--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@ from typing import Optional, Dict, List, Any
 from datetime import datetime
 from enum import Enum
 
-class Platfrom(str, Enum):
+class Platform(str, Enum):
     INSTAGRAM = "instagram"
     TWITTER = "twitter"
     TIKTOK = "tiktok"
@@ -25,4 +25,29 @@ class UserResponse(BaseModel):
     full_name:Optional[str]
     is_premiun: bool
     created_at: datetime
+    
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    
+    
+class SocialAccountConnect(BaseModel):
+    platform: Platform
+    access_token: str
+    platform_user_id: str
+    username:Optional[str] = None
+    
+class VoiceQuery(BaseModel):
+    audio_data: Optional[str] = None
+    text: Optional[str] = None
+    session_id: str
+    
+class VoiceResponse(BaseModel):
+    text:str
+    audio_url: Optional[str] = None
+    actions: List[Dict[str, Any]] = []
+    
     

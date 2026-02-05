ca4a60bb8cfa889dd9f42410e0dfe6f7ceb3c847
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-05 21:14:34 +0530

    ai

--- pika_agent.py
+++ pika_agent.py
@@ -13,4 +13,27 @@ class PikaAI:
         
         self.conversation_history = []
         
-    
+    def process_query(self, user_input: str, context:Dict[str, Any]) -> Dict[str, Any]:
+        #Process user voice or text
+        
+        user_input_lower = user_input.lower() 
+        
+        if any(word in user_input_lower for word in ["what's up", "whats up", "what up", "sup", "briefing"]):
+            return self._hadle_breifing(context)
+        
+        elif any(word in user_input_lower for word in ["post", "share", "upload"]):
+            return self._handle_post(user_input, context)
+        
+        elif any(word in user_input_lower for word in ["dm", "message", "messages"]):
+            return self._handle_dm_inetnet(user_input, context)
+        
+        elif any(word in user_input_lower for word in ["check", "look", " see", "show"]):
+            return self._handle_check_intent(user_input, context)
+        
+        elif "comment" in user_input_lower:
+            return self._handle_comment_intent(user_input, context)
+        
+        else:
+            return self._handle_general_query(user_input, context)
+        
+        

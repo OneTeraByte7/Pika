e3d9d3af595ac706cbd95ffa6830f63f4b211311
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-11 20:52:42 +0530

    dependancies

--- server/app/auth.py
+++ server/app/auth.py
@@ -7,3 +7,11 @@ from passlib.context import CryptoContext
 from typing import Optional
 
 from models.schemas import UserCreate, UserLogin, UserResponse, Token
+from models.database import User
+from config.settings import settings
+router = APIRouter(prefix = "/auth", tags = ["authentication"])
+
+pwd_context  =CryptoContext(schemas = ["bcrypt"], deprecates = "auto")
+oauth2_scheme = OAuth2PasswordBearer(toeknUrl = "auth/login")
+
+def

d1e94e1d301c1a60c31b2dbb80abf49d711eee03
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-11 20:48:11 +0530

    making

---
+++ server/app/auth.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptoContext
+from typing import Optional
+
+from models.schemas import UserCreate, UserLogin, UserResponse, Token

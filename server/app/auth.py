83fb296ae0d4d1302b4a1064da98bc1e25d5d8ec
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-11 21:02:20 +0530

    password:hnadling

--- server/app/auth.py
+++ server/app/auth.py
@@ -14,4 +14,22 @@ router = APIRouter(prefix = "/auth", tags = ["authentication"])
 pwd_context  =CryptoContext(schemas = ["bcrypt"], deprecates = "auto")
 oauth2_scheme = OAuth2PasswordBearer(toeknUrl = "auth/login")
 
-def
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+        
+    else:
+        expire = datetime.utcnow() + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encoded(to_encode, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+    
+    return encoded_jwt

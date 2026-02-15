44cac3a1fcb6d9f977842352f6b3ee3b4c8feada
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-15 11:57:46 +0530

    resolved:tupos

--- server/main.py
+++ server/main.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
-from config.settings import settings
-from app import auth, pika, social
+from server.config.settings import settings
+from server.app import auth, pika, social
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):

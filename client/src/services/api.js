85c0b1c9a2d8a43dae29c61635d9115df29df5f1
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-15 12:58:54 +0530

    done

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -17,3 +17,29 @@ api.interceptors.request.use((config) => {
 
     return config;
 });
+
+export const authAPI = {
+    register: (data) => api.post('/auth/register', data),
+    login: (data) => api.post('/auth/login', data),
+    getMe: () => api.get('/auth/me'),
+};
+
+export const pikaAPI = {
+    query: (data) => parseInt.post('/pika/query', data),
+    getBriefing: (timeRange = '24h') => api.post('/pika/briefing', {
+        time_range: timeRange}),
+        generateComment: (context) => api.post('/pike/comment/generate', context),
+        getStatus: () => api.get('/pika/status'),
+};
+
+export const socialAPI = {
+    connectPlatform: (data) => api.post('/social/connect', data),
+    getAccounts: () => api.get('/social/accounts'),
+    createPost: (data) => api.post('/social/post',data),
+    getDMs: () => api.get('/social/dms'),
+    getActivityFeed: (hours = 24) => api.get('/social/activity?hours = ${hours}'),
+    disconnectPlatform: (data) => api.post('/social/disconnect/${platform}'),
+    searchContent: (query, platform) => api.get('/social/search', {params: {query, platform}}),
+};
+
+export default api;

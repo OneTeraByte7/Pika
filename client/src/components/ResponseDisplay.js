942f0a935a77086d57e8afa325960fc1579f5395
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-02-18 20:35:53 +0530

    logic:built

--- client/src/components/ResponseDisplay.js
+++ client/src/components/ResponseDisplay.js
@@ -2,4 +2,13 @@ import { motion} from 'framer-motion';
 import { MessageCircle, ThumbsUp, Send } from 'lucide-react';
 import { UsePikaStore } from '../store';
 
-export default fun
+export default function ResponseDisplay()
+{
+    const { response } = usePikaStore();
+
+    if(!response) return null;
+
+    return (
+        
+    )
+}

066eb3e374f315b95a1e6631876143a91a32c42c
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-04-16 21:13:39 +0530

    texting:feature

---
+++ client/src/pages/dating/messages.tsx
@@ -0,0 +1,8 @@
+export default function Messages() {
+  return (
+    <div className="max-w-3xl mx-auto py-16 px-6">
+      <h1 className="text-3xl font-black text-white mb-4">Messages</h1>
+      <p className="text-white/70 mb-6">Chat flows will be added later. This is a placeholder.</p>
+    </div>
+  );
+}

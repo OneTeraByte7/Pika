86eb63b07ed3a9a86e052df8ac26802ee9754a8a
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-04-21 21:19:05 +0530

    banner

---
+++ client/src/components/social/twitter/TwitterBanner.tsx
@@ -0,0 +1,8 @@
+export default function TwitterBanner() {
+    return (
+        <div className="p-6 bg-white/5 rounded-2xl">
+            <h3 className="font-bold">Twitter Insights</h3>
+            <p className="text-sm text-white/40 mt-2">Realtime engagement and trends from your connected Twitter accounts.</p>
+        </div>
+    );
+}
